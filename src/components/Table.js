e785c4f2fe46e30651379d1ab3361f03b272c091
Matthew Ching <matthewaching@gmail.com>
2022-07-29 15:52:00 -0500

    Create main skeleton elements for landing page

---
+++ src/components/Table.js
@@ -0,0 +1,24 @@
+function Table(props) {
+    return <table>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+    </table>
+}
+
+export default Table;
