2cdfa0899e02eb52c0705a6dc3606bcee47d7cb0
Matthew Ching <matthewaching@gmail.com>
2022-07-29 18:12:54 -0500

    Implement base input to table functionality
    Submitting item from input currently adds the item to a table
    underneath the input line. Next step will be to add multiple subfields
    to add properties to each entry.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,28 @@
-function Searchbar(props) {
+function Searchbar({currentItem, setCurrentItem, itemArray, setArray}) {
+    
+    const searchInput = (e) => {
+        setCurrentItem({
+            name: e.target.value,
+            index: currentItem.index,
+        });
+    }
+
+    const addItem = (e) => {
+        e.preventDefault();
+        setArray([...itemArray, currentItem]);
+        setCurrentItem({
+            name: '',
+            index: currentItem.index + 1,
+        });
+    }
+
     return(
         <div>
-            <label for="addItem">Item Name:</label>
-            <input type="text" id="addItem"></input>
-            <button type="submit">Add</button>
+            <form onSubmit={addItem}>
+                <label htmlFor="addItem">Item Name:</label>
+                <input type="text" id="addItem" value={currentItem.name} onChange={searchInput}></input>
+                <button type="submit">Add</button>
+            </form>
         </div>
     )
 }
