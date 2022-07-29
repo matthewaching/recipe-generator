0ba0382c9177085867035471970f65cbc8945173
Matthew Ching <matthewaching@gmail.com>
2022-07-29 18:54:50 -0500

    Change array logic to useEffect hook
    This change improves readability of the code and simplifies the
    onSubmit functionality. With scaling of the project to implement a REST
    API, this simplicity should prove to be important.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,21 @@
 function Table({itemArray}) {
     return <table>
-        {itemArray.map(item => {
-            return <tr><td>{item.name}</td></tr>
-        })}
+        <thead>
+            <tr>
+                <td>Item Number</td>
+                <td>Item Name</td>
+            </tr>
+        </thead>
+        <tbody>
+            {itemArray.map(item => {
+                return (
+                    <tr>
+                        <td>{item.index}</td>
+                        <td>{item.name}</td>
+                    </tr>
+                );
+            })}
+        </tbody>
     </table>
 }
 
