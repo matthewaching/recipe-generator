24b6ef75603fa3bbd91d38f4ca6e44555db517a3
Matthew Ching <matthewaching@gmail.com>
2022-07-30 18:35:19 -0500

    Implement multiple input fields with style updates
    Item state updated for additional object keys. Formatting of Date and
    Cooked fields will need to be done next as well as a barebones back-end
    to start storing data.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import './App.css';
-import { useState } from 'react';
-import SearchBar from './components/SearchBar';
-import Table from './components/Table';
+import "./App.css";
+import { useState } from "react";
+import InputList from "./components/InputList/InputList";
 
 function App() {
-  const [currentItem, setCurrentItem] = useState({
-      name: '',
-      index: 0,
-    },
-  );
-
-  const [itemArray, setArray] = useState([]);
-
   return (
     <div className="App">
-      <SearchBar
-        currentItem={currentItem}
-        setCurrentItem={setCurrentItem}
-        itemArray={itemArray}
-        setArray={setArray}
-      />
-      <Table
-        currentItem={currentItem}
-        setCurrentItem={setCurrentItem}
-        itemArray={itemArray}
-        setArray={setArray}
-      />
+      <InputList />
     </div>
   );
 }
