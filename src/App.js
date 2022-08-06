7c784daae784ed4ca4e426b042d52169a8c922a0
Matthew Ching <matthewaching@gmail.com>
2022-08-06 15:35:03 -0500

    Add title for page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import InputList from "./components/InputList/InputList";
+import Title from "./components/Title/Title";
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <InputList />
     </div>
   );
