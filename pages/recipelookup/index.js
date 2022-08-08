9867a7a620e63314551f52d71c9eb1175cf255a8
Matthew Ching <matthewaching@gmail.com>
2022-08-08 17:26:51 -0500

    Implement fetch API to random generator
    The current iteration of the fetch API simply displays a recipe name
    from the random fetch. Integrating additional details should be a simple
    matter of extending the json result to its other contents.

--- pages/recipelookup/index.js
+++ pages/recipelookup/index.js
@@ -1,9 +1,11 @@
 import Title from "../../src/components/recipelookup/Title";
+import DisplayRecipe from "../../src/components/recipelookup/DisplayRecipe";
 
 export default function RecipeLookup() {
   return (
     <div className="App">
       <Title />
+      <DisplayRecipe />
     </div>
   );
 }
