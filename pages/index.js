e86eddb0cda54d6769f92881ca0f897cc6687e77
Matthew Ching <matthewaching@gmail.com>
2022-08-08 18:37:29 -0500

    Compose introduction on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my page!</h1>
+      <h1>Welcome to my Food App!</h1>
+      <h2>This app consists of two related projects:</h2>
+      <ol>
+        <li>
+          Matt's Food Memories - a database record of all memorable dishes I've
+          eaten or cooked
+        </li>
+        <li>
+          Random Recipe Generator - a tool that queries a recipe database and
+          returns a random entry
+        </li>
+      </ol>
+      <h4>
+        This app was constructed with React running on Next.js. Material UI
+        libraries were utilized for frontend design, and the app is hosted on
+        Firebase.
+      </h4>
     </div>
   );
 }
